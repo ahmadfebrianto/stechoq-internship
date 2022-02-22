beb97571dfd902b13ea84b73aa56ab005f3f1a60
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-02-22 16:27:48 +0700

    Android: Rename files

--- Android/Project1/app/src/main/java/com/example/project1/ui/layout1/Layout1Fragment.kt
+++ Android/Project1/app/src/main/java/com/example/project1/ui/layout1/Layout1Fragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.project1.databinding.FragmentHomeBinding
+import com.example.project1.databinding.FragmentLayout1Binding
 
-class HomeFragment : Fragment() {
+class Layout1Fragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentLayout1Binding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLayout1Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
 //        val textView: TextView = binding.textHome
--- Android/Project1/app/src/main/java/com/example/project1/ui/layout2/Layout2Fragment.kt
+++ Android/Project1/app/src/main/java/com/example/project1/ui/layout2/Layout2Fragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.project1.databinding.FragmentDashboardBinding
+import com.example.project1.databinding.FragmentLayout2Binding
 
-class DashboardFragment : Fragment() {
+class Layout2Fragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentLayout2Binding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentLayout2Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
 //        val textView: TextView = binding.textDashboard
